1b502881b01365a00d3aa73274c5cf40aeeb8859
Motoki Kikuchi <wildflower.pink0102@gmail.com>
2020-05-22 17:58:40 +0900

    Add sample programs

---
+++ ch01/work/for.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+	// for ;;
+	// {
+
+	// for ;
+	// ;
+	// {
+
+	// for
+	// ;
+	// ;
+	// {
+
+	// for
+	// ; true ;
+	// {
+
+	// for
+	// i := 0; i < 1 ;
+	// {
+
+	// for
+	// i := 0; i < 1
+	// {
+
+	// NG
+	for
+	i := 0; i < 1; i++
+	{
+		fmt.Println("hello")
+		break;
+	}
+}
+
+
+
