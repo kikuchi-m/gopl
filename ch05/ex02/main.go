3c04788ebfe1067e84427895e61daf7a82d28e4c
Motoki Kikuchi <wildflower.pink0102@gmail.com>
2020-08-23 21:21:55 +0900

    Add excercise 5.2

---
+++ ch05/ex02/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
+		os.Exit(1)
+	}
+	for elms, count := range visit(make(map[string]int), doc) {
+		fmt.Println(elms, count)
+	}
+}
+
+func visit(elms map[string]int, n *html.Node) map[string]int {
+	if n == nil {
+		return elms
+	}
+	if n.Type == html.ElementNode {
+		elms[n.Data]++
+	}
+	return visit(visit(elms, n.FirstChild), n.NextSibling)
+}
