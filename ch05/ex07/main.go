b2ee925b4e3b392105c1465489d9ab8e16d3b889
Motoki Kikuchi <wildflower.pink0102@gmail.com>
2020-08-25 20:58:00 +0900

    Fix messages

--- ch05/ex07/main.go
+++ ch05/ex07/main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	doc, err := html.Parse(os.Stdin)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
+		fmt.Fprintf(os.Stderr, "pretty print: %v\n", err)
 		os.Exit(1)
 	}
 	forEachNode(doc, startElement, endElement)
